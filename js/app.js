2a4d29d193dd22e13eda2dcef021a97f8dbad1d2
willmcneilly <willmcneilly@me.com>
2015-06-23 18:32:09 +0100

    scaffold out pomodoroSetStore

--- js/app.js
+++ js/app.js
@@ -11,8 +11,26 @@ var appSettings = {
   numberPomodorosInSet: 8,
   pomodoroTime: 25,
   shortBreakTime: 5,
-  longBreakTime: 15 
+  longBreakTime: 15
 }
+
+
+pomodoroSetStore = Reflux.createStore({
+  getIntialState() {
+    return {
+      periods: [
+        {id: 1, type: 'pomodoro', length: 60000, status: 'inactive'},
+        {id: 2, type: 'pomodoro', length: 60000, status: 'inactive'},
+        {id: 3, type: 'short-break', length: 10000, status: 'inactive'},
+        {id: 4, type: 'pomodoro', length: 60000, status: 'inactive'},
+        {id: 5, type: 'pomodoro', length: 60000, status: 'inactive'},
+        {id: 6, type: 'long-break', length: 20000, status: 'inactive'}
+      ],
+      queuedPeriodID: null,
+      currentTime: null,
+    }
+  }
+})
 
 let AppContainer = React.createClass({
   render() {
